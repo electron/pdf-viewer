c99278fc5a4c0cf9e225b401cbec65a176c991fc
deepak1556 <hop2deep@gmail.com>
2017-01-18 22:05:23 +0530

    add webui message handlers

--- main.js
+++ main.js
@@ -10,27 +10,26 @@
  */
 var viewer;
 
-
 (function() {
   /**
-   * Stores any pending messages received which should be passed to the
-   * PDFViewer when it is created.
-   * @type Array
-   */
+  * Stores any pending messages received which should be passed to the
+  * PDFViewer when it is created.
+  * @type Array
+  */
   var pendingMessages = [];
 
   /**
-   * Handles events that are received prior to the PDFViewer being created.
-   * @param {Object} message A message event received.
-   */
+  * Handles events that are received prior to the PDFViewer being created.
+  * @param {Object} message A message event received.
+  */
   function handleScriptingMessage(message) {
     pendingMessages.push(message);
   }
 
   /**
-   * Initialize the global PDFViewer and pass any outstanding messages to it.
-   * @param {Object} browserApi An object providing an API to the browser.
-   */
+  * Initialize the global PDFViewer and pass any outstanding messages to it.
+  * @param {Object} browserApi An object providing an API to the browser.
+  */
   function initViewer(browserApi) {
     // PDFViewer will handle any messages after it is created.
     window.removeEventListener('message', handleScriptingMessage, false);
@@ -40,16 +39,14 @@ var viewer;
   }
 
   /**
-   * Entrypoint for starting the PDF viewer. This function obtains the browser
-   * API for the PDF and constructs a PDFViewer object with it.
-   */
-  function main() {
+  * Entrypoint for starting the PDF viewer. This function obtains the browser
+  * API for the PDF and constructs a PDFViewer object with it.
+  */
+  cr.sendWithPromise('initialize').then(function(opts) {
     // Set up an event listener to catch scripting messages which are sent prior
     // to the PDFViewer being created.
     window.addEventListener('message', handleScriptingMessage, false);
 
-    createBrowserApi().then(initViewer);
-  };
-
-  main();
-})();
+    createBrowserApi(opts).then(initViewer);
+  });
+})()
